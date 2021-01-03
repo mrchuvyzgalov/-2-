7cea220afe12f96a1d9e5702a90ab92b65b63645
Kirill Chuvyzgalov <59579234+mrchuvyzgalov@users.noreply.github.com>
2021-01-03 12:26:00 +0300

    Changed TestFunction

--- src/test_decision.cpp
+++ src/test_decision.cpp
@@ -12,113 +12,115 @@ void test_decision::TestAlldecisions() {
 }
 
 void test_decision::TestFunction(const DecisionClass& class_) {
-    {                                                      
-        Deductions<int> d = decision(1, 2, 5, class_);      
-                                                           
-        std::vector<int> v1 = { 2 };                       
-                                                           
-        ASSERT_EQUAL(d.get_leftovers(), v1);               
-        ASSERT_EQUAL(d.get_module(), 5);                   
-    }                                                      
-                                                           
-    {                                                      
-        Deductions<int> d = decision(12, 8, 48, class_);   
-                                                           
-        std::vector<int> v1 = { };                         
-                                                           
-        ASSERT_EQUAL(d.get_leftovers(), v1);               
-        ASSERT_EQUAL(d.get_module(), 48);                   
-    }                                                      
-                                                           
-    {                                                      
-        Deductions<int> d = decision(9, 3, 12, class_);     
-                                                           
-        std::vector<int> v1 = { 3, 7, 11};                 
-                                                           
-        ASSERT_EQUAL(d.get_leftovers(), v1);               
-        ASSERT_EQUAL(d.get_module(), 12);                  
-    }                                                      
-                                                           
-    {                                                      
-        Deductions<int> d = decision(-3, 3, 12, class_);    
-                                                           
-        std::vector<int> v1 = { 3, 7, 11};                 
-                                                           
-        ASSERT_EQUAL(d.get_leftovers(), v1);               
-        ASSERT_EQUAL(d.get_module(), 12);                  
-    }      
+    {
+        Deductions<int> d = decision(1, 2, 5, class_);
 
-    {                                                      
-        Deductions<int> d = decision(-3, 4, 12, class_);    
-                                                           
-        std::vector<int> v1 = { };                 
-                                                           
-        ASSERT_EQUAL(d.get_leftovers(), v1);               
-        ASSERT_EQUAL(d.get_module(), 12);                  
-    }   
+        ASSERT_EQUAL(d.get_leftover(), 2);
+        ASSERT_EQUAL(d.get_step(), 5);
+        ASSERT_EQUAL(d.get_module(), 5);
+        ASSERT(d.get_hasDecision());
+    }
 
-    {                                                      
-        Deductions<int> d = decision(-4, 2, 5, class_);      
-                                                           
-        std::vector<int> v1 = { 2 };                       
-                                                           
-        ASSERT_EQUAL(d.get_leftovers(), v1);               
-        ASSERT_EQUAL(d.get_module(), 5);                   
-    }    
-                                               
-    {                                                      
-        Deductions<int> d = decision(-3, -9, 12, class_);   
-                                                           
-        std::vector<int> v1 = { 3, 7, 11 };                 
-                                                           
-        ASSERT_EQUAL(d.get_leftovers(), v1);               
-        ASSERT_EQUAL(d.get_module(), 12);                  
-    }     
+    {
+        Deductions<int> d = decision(12, 8, 48, class_);
 
-    {                                                      
-        Deductions<int> d = decision(-3, -8, 12, class_);    
-                                                           
-        std::vector<int> v1 = { };                 
-                                                           
-        ASSERT_EQUAL(d.get_leftovers(), v1);               
-        ASSERT_EQUAL(d.get_module(), 12);                  
-    }    
+        ASSERT_EQUAL(d.get_leftover(), 0);
+        ASSERT_EQUAL(d.get_step(), 0);
+        ASSERT_EQUAL(d.get_module(), 48);
+        ASSERT(!d.get_hasDecision());
+    }
 
-    {                                                      
-        Deductions<int> d = decision(-4, -3, 5, class_);      
-                                                           
-        std::vector<int> v1 = { 2 };                       
-                                                           
-        ASSERT_EQUAL(d.get_leftovers(), v1);               
-        ASSERT_EQUAL(d.get_module(), 5);                   
-    }                                                   
-                                                           
-    {                                                      
-        Deductions<int> d = decision(9, -9, 12, class_);    
-                                                           
-        std::vector<int> v1 = { 3, 7, 11};                 
-                                                           
-        ASSERT_EQUAL(d.get_leftovers(), v1);               
-        ASSERT_EQUAL(d.get_module(), 12);                  
-    }     
+    {
+        Deductions<int> d = decision(9, 3, 12, class_);
 
-    {                                                      
-        Deductions<int> d = decision(9, -8, 12, class_);    
-                                                           
-        std::vector<int> v1 = { };                 
-                                                           
-        ASSERT_EQUAL(d.get_leftovers(), v1);               
-        ASSERT_EQUAL(d.get_module(), 12);                  
-    }    
+        ASSERT_EQUAL(d.get_leftover(), 3);
+        ASSERT_EQUAL(d.get_step(), 4);
+        ASSERT_EQUAL(d.get_module(), 12);
+        ASSERT(d.get_hasDecision());
+    }
 
-    {                                                      
-        Deductions<int> d = decision(1, -3, 5, class_);      
-                                                           
-        std::vector<int> v1 = { 2 };                       
-                                                           
-        ASSERT_EQUAL(d.get_leftovers(), v1);               
-        ASSERT_EQUAL(d.get_module(), 5);                   
-    }                                                        
+    {
+        Deductions<int> d = decision(-3, 3, 12, class_);
+
+        std::vector<int> v1 = { 3, 7, 11 };
+
+        ASSERT_EQUAL(d.get_leftover(), 3);
+        ASSERT_EQUAL(d.get_step(), 4);
+        ASSERT_EQUAL(d.get_module(), 12);
+        ASSERT(d.get_hasDecision());
+    }
+
+    {
+        Deductions<int> d = decision(-3, 4, 12, class_);
+
+        ASSERT_EQUAL(d.get_leftover(), 0);
+        ASSERT_EQUAL(d.get_step(), 0);
+        ASSERT_EQUAL(d.get_module(), 12);
+        ASSERT(!d.get_hasDecision());
+    }
+
+    {
+        Deductions<int> d = decision(-4, 2, 5, class_);
+
+        ASSERT_EQUAL(d.get_leftover(), 2);
+        ASSERT_EQUAL(d.get_step(), 5);
+        ASSERT_EQUAL(d.get_module(), 5);
+        ASSERT(d.get_hasDecision());
+    }
+
+    {
+        Deductions<int> d = decision(-3, -9, 12, class_);
+
+        ASSERT_EQUAL(d.get_leftover(), 3);
+        ASSERT_EQUAL(d.get_step(), 4);
+        ASSERT_EQUAL(d.get_module(), 12);
+        ASSERT(d.get_hasDecision());
+    }
+
+    {
+        Deductions<int> d = decision(-3, -8, 12, class_);
+
+        ASSERT_EQUAL(d.get_leftover(), 0);
+        ASSERT_EQUAL(d.get_step(), 0);
+        ASSERT_EQUAL(d.get_module(), 12);
+        ASSERT(!d.get_hasDecision());
+    }
+
+    {
+        Deductions<int> d = decision(-4, -3, 5, class_);
+
+        ASSERT_EQUAL(d.get_leftover(), 2);
+        ASSERT_EQUAL(d.get_step(), 5);
+        ASSERT_EQUAL(d.get_module(), 5);
+        ASSERT(d.get_hasDecision());
+    }
+
+    {
+        Deductions<int> d = decision(9, -9, 12, class_);
+
+        ASSERT_EQUAL(d.get_leftover(), 3);
+        ASSERT_EQUAL(d.get_step(), 4);
+        ASSERT_EQUAL(d.get_module(), 12);
+        ASSERT(d.get_hasDecision());
+    }
+
+    {
+        Deductions<int> d = decision(9, -8, 12, class_);
+
+        ASSERT_EQUAL(d.get_leftover(), 0);
+        ASSERT_EQUAL(d.get_step(), 0);
+        ASSERT_EQUAL(d.get_module(), 12);
+        ASSERT(!d.get_hasDecision());
+    }
+
+    {
+        Deductions<int> d = decision(1, -3, 5, class_);
+
+        ASSERT_EQUAL(d.get_leftover(), 2);
+        ASSERT_EQUAL(d.get_step(), 5);
+        ASSERT_EQUAL(d.get_module(), 5);
+        ASSERT(d.get_hasDecision());
+    }
 }
 
 void test_decision::TestEuler() {
